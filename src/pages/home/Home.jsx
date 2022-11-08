b258926c2c5149fc0c17786f19bfbee0c5d84119
kamau7314 <57088508+kamau7314@users.noreply.github.com>
2022-11-08 22:05:32 +0300

    latestCommit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Featured from "../../components/featured/Featured";
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import MailList from "../../components/mailList/MailList";
 import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
 
@@ -16,6 +17,7 @@ const Home = () => {
         <PropertyList/>
         <h1 className="homeTitle">Home Guest Love</h1>
         <FeaturedProperties/>
+        <MailList/>
       </div>
     </div>
   )
